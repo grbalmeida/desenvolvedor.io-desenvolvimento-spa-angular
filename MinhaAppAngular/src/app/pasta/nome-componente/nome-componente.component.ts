fbaeab6147115a9b264c373166ecbf6f666225f5
gilvan <g.r.almeida@live.com>
2020-04-12 03:26:57 -0300

    Componentes

---
+++ MinhaAppAngular/src/app/pasta/nome-componente/nome-componente.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nome-componente',
+  templateUrl: './nome-componente.component.html'
+})
+export class NomeComponenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
